dcbfb69b3eb55dc792abb6e728180b403e9f926f
Alexander <siegfried1997@gmail.com>
2022-07-15 01:39:15 +0400

    refactoring

--- SimbirSoftTechnicalTask.Controller/ExtractPlainTextFromHtmlPage.cs
+++ SimbirSoftTechnicalTask.Controller/ExtractPlainTextFromHtmlPage.cs
@@ -16,27 +16,41 @@ namespace SimbirSoftTechnicalTask.Controller
 		/// <param name="htmlPage">text including HtmlPage</param>
 		/// <returns></returns>
 		public static string Start(string htmlPage)
-		{
-			if (htmlPage == null) throw new ArgumentNullException(nameof(htmlPage));
-			var doc = new HtmlDocument();
-			doc.LoadHtml(htmlPage);
-			var chunks = new List<string>();
-			var nodes = doc.DocumentNode.SelectSingleNode("//body").DescendantsAndSelf();
-			foreach (var node in nodes)
-			{
-				if (node.NodeType == HtmlNodeType.Text && node.ParentNode.Name != "script")
-				{
-					chunks.Add(node.InnerText.ToLower());
-				}
-			}
-			var arraySplitwords = String.Join(" ", chunks).Split(new[] {' ', ',', '.', '!', '?','"', ';', ':', '[', ']', '(', ')', '\n',
-				'\r', '\t'
-			});
-			RegexOptions options = RegexOptions.None;
-			Regex regex = new Regex("[ ]{2,}", options);
-			var words = regex.Replace(string.Join(" ", arraySplitwords), " ");
+        {
+            List<string> chunks = GetInnerTextFromHTMLNodes(htmlPage);
+            var arraySplitwords = String.Join(" ", chunks).Split(new[] {' ', ',', '.', '!', '?','"', ';', ':', '[', ']', '(', ')', '\n',
+                '\r', '\t'
+            });
+            RegexOptions options = RegexOptions.None;
+            Regex regex = new Regex("[ ]{2,}", options);
+            var words = regex.Replace(string.Join(" ", arraySplitwords), " ");
 
-			return words;
-		}
-	}
+            return words;
+
+            
+        }
+        static List<string> GetInnerTextFromHTMLNodes(string htmlPage)
+        {
+            List<string> chunks = new List<string>();
+            IEnumerable<HtmlNode> nodes;
+            GetNodesFromHTMLPage(htmlPage, out nodes);
+            foreach (var node in nodes)
+            {
+                if (node.NodeType == HtmlNodeType.Text && node.ParentNode.Name != "script")
+                {
+                    chunks.Add(node.InnerText.ToLower());
+                }
+            }
+
+            return chunks;
+        }
+
+        private static void GetNodesFromHTMLPage(string htmlPage, out IEnumerable<HtmlNode> nodes)
+        {
+            if (htmlPage == null) throw new ArgumentNullException(nameof(htmlPage));
+            var doc = new HtmlDocument();
+            doc.LoadHtml(htmlPage);
+            nodes = doc.DocumentNode.SelectSingleNode("//body").DescendantsAndSelf();
+        }
+    }
 }
